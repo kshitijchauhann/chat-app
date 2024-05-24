0dd3851745bf0f8c5dbca7c06e94749a0f26a7b0
username <chauhankay18@gmail.com>
2024-05-24 20:51:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,56 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 const FirstInterface = () => (
   <> 
-    <Container maxWidth="sm">
-      <Box sx={{m: 5}}>
+    <Box sx={{m: 5}}>
       This Box uses MUI System props for quick customization.
       <Button>Click Here to generate code</Button>
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      Paste your code here</TextField>
+      <TextField id="filled-basic" label="Paste your code here" variant="filled" />
     </Box>
-    </Container>
   </>
 );
 
 const SecondInterface = () => (
   <>
-
+    <Container maxWidth="sm">
+          <Box
+      sx={{ 
+        width: '45vh',
+        height: '70vh',
+        border: '2px solid grey',
+        margin: '20px 10px 10px 27px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+      }}
+    >
+    <Grid 
+      container 
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Box>
+       <TextField id="filled-basic" label="Enter here.." variant="filled"/>      
+      </Box>
+      <Box sx={{
+        width: '10vh',
+        height: '4vh',
+      }}>
+      <Button size="large"><DoubleArrowIcon></DoubleArrowIcon></Button>
+      </Box>
+  </Grid>
+</Box>
+</Container>
   </>
 );
 
 function App() {
-  return <FirstInterface />;
+  return <SecondInterface />;
 }
 
 export default App;
